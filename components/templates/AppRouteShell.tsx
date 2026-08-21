72d561a22397c4f1c0d33d62812f9d9943143a89
headdrop <ehdgmltn@gmail.com>
2026-08-21 15:21:23 +0900

    Feature: 복사 폴백과 프레임 안 shadcn 토스트 골격을 추가한다

--- components/templates/AppRouteShell.tsx
+++ components/templates/AppRouteShell.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { MobileDeviceFrame } from "@/components/organisms/MobileDeviceFrame";
+import { AppToaster } from "@/components/providers/AppToaster";
 import type { ReactNode } from "react";
 
 type AppRouteShellProps = {
@@ -11,5 +12,9 @@ type AppRouteShellProps = {
  * 실제 모바일은 풀스크린, PC는 Figma(390)와 같은 폰 프레임으로 미리봅니다.
  */
 export function AppRouteShell({ children }: AppRouteShellProps) {
-  return <MobileDeviceFrame>{children}</MobileDeviceFrame>;
+  return (
+    <MobileDeviceFrame>
+      <AppToaster>{children}</AppToaster>
+    </MobileDeviceFrame>
+  );
 }
